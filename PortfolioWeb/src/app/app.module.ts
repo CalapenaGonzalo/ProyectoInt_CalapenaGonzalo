5b923d86efd82c09effa5c24bd7dec7603305f68
CalapenaGonzalo <100537327+CalapenaGonzalo@users.noreply.github.com>
2022-08-17 20:02:43 -0300

    creacion de header

--- PortfolioWeb/src/app/app.module.ts
+++ PortfolioWeb/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesSocialesComponent
   ],
   imports: [
     BrowserModule
