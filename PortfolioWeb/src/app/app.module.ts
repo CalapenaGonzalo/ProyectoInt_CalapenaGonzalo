ad178c45bd7272596dbed0d74af032d1d95e9690
CalapenaGonzalo <100537327+CalapenaGonzalo@users.noreply.github.com>
2022-08-18 08:51:46 -0300

    creacion de acerca de

--- PortfolioWeb/src/app/app.module.ts
+++ PortfolioWeb/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
 import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesSocialesComponent
+    RedesSocialesComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
