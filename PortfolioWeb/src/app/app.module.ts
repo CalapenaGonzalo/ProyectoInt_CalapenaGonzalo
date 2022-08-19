864e61055970631dfda7811453c7bf4bbd9b860b
CalapenaGonzalo <100537327+CalapenaGonzalo@users.noreply.github.com>
2022-08-19 19:04:17 -0300

    arreglo del header

--- PortfolioWeb/src/app/app.module.ts
+++ PortfolioWeb/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
-import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
@@ -19,7 +18,6 @@ import { FooterComponent } from './componentes/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesSocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
